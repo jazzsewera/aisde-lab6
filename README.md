107c87d1e5739286de9549acf12e8bbe7c857cfb
Blazej Sewera <code@sewera.dev>
2019-06-14 21:53:12 +0200

    Plotting utilities

---
+++ README.md
@@ -0,0 +1,6 @@
+Utilities for plotting Simulated Annealing algorithm.
+Use plot.py with -t option to put algorithm pass rather than temperature on the x axis.
+
+Everything mine under MIT License. Files in Sources/ directory are not mine
+and authors are written in comments in the code. They are not under any specified license, 
+so please use them only if you have a permission.
---
+++ plot.py
@@ -0,0 +1,59 @@
+import csv
+import sys
+import getopt
+import numpy as np
+from datetime import datetime
+try:
+    from matplotlib import pyplot as plt
+except Exception:
+    import matplotlib
+    matplotlib.use('pdf')
+    from matplotlib import pyplot as plt
+
+
+def save_plot(ax, name='plot', const_temp=False):
+    if not const_temp:
+        ax.set_xscale('log')
+        plt.xlabel('Temperature')
+    else:
+        plt.xlabel('Algorithm pass no. (const. temp.)')
+    dt = datetime.now().strftime('%H-%M-%S')
+    plt.ylabel('Cost')
+    plt.savefig('out/{0}_{1}.pdf'.format(name, dt))
+
+
+def plot_simul_annealing(const_temp=False):
+    temp_array = []
+    pos_array = []
+    with open('cooling.csv', 'r', newline='') as cooling_file:
+        reader = csv.reader(cooling_file, delimiter=';')
+        for row in reader:
+            temp_array.append(float(row[0]))
+            pos_array.append(int(row[1]))
+
+    ax = plt.axes()
+
+    if const_temp:
+        ax.plot(np.arange(len(pos_array)), pos_array, ',')
+    else:
+        ax.plot(temp_array, pos_array, ',')
+        ax.invert_xaxis()
+
+    save_plot(ax, 'temp_pos', const_temp)
+
+
+def main(argv):
+    const_temp = False
+    try:
+        opts, args = getopt.getopt(argv, 't')
+    except getopt.GetoptError:
+        exit()
+    for opt, arg in opts:
+        if opt == '-t':
+            const_temp = True
+
+    plot_simul_annealing(const_temp)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
